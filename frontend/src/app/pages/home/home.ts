280dcb3b95f722b56ce46807a044dbc89c8037cf
Johan Rodríguez <jrodriguez0612@uta.edu.ec>
2026-02-26 11:37:54 -0500

    Se agregó la sección de metricas y se mejoro el diseño de la sección nosotros

--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -3,11 +3,12 @@ import { HeroComponent } from '../../components/hero/hero.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { NosotrosComponent } from '../../components/nosotros/nosotros.component';
 import { WhatsAppButtonComponent } from '../../components/whatsapp-button/whatsapp-button.component';
+import { MetricsComponent } from '../../components/metrics/metrics.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent, FooterComponent, NosotrosComponent, WhatsAppButtonComponent],
+  imports: [HeroComponent, MetricsComponent, FooterComponent, NosotrosComponent, WhatsAppButtonComponent],
   templateUrl: './home.html',
   styleUrl: './home.sass',
 })
