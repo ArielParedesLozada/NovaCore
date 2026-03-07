5e318d596fb484753c9e16a336888d5c2935265d
Diego200509 <diegojijon2005@gmail.com>
2026-03-07 14:08:46 -0500

    style: mejora en como se ve la pagina de inicio y el logo

--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -7,19 +7,21 @@ import { MetricsComponent } from '../../components/metrics/metrics.component';
 import { FeaturedServicesComponent } from '../../components/featured-services/featured-services.component';
 import { FeaturedProjectsComponent } from '../../components/featured-projects/featured-projects.component';
 import { FeaturedBlogsComponent } from '../../components/featured-blogs/featured-blogs.component';
+import { ScrollRevealDirective } from '../../shared/directives/scroll-reveal.directive';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    HeroComponent, 
-    MetricsComponent, 
+    ScrollRevealDirective,
+    HeroComponent,
+    MetricsComponent,
     FeaturedServicesComponent,
     FeaturedProjectsComponent,
     FeaturedBlogsComponent,
-    FooterComponent, 
-    NosotrosComponent, 
-    WhatsAppButtonComponent
+    FooterComponent,
+    NosotrosComponent,
+    WhatsAppButtonComponent,
   ],
   templateUrl: './home.html',
   styleUrl: './home.sass',
